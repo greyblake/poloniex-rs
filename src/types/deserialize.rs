27ba4de07befc1eddd51dd6b23ca9bd14267b009
Sergey Potapov <blake131313@gmail.com>
2017-08-18 23:31:47 +0200

    Cleanup

--- src/types/deserialize.rs
+++ src/types/deserialize.rs
@@ -1,16 +1,22 @@
 use serde::de::{self, Deserialize};
 use chrono::naive::NaiveDateTime;
 
-pub fn string_to_f64<'de, D>(d: D) -> Result<f64, D::Error> where D: de::Deserializer<'de> {
+pub fn string_to_f64<'de, D>(d: D) -> Result<f64, D::Error>
+    where D: de::Deserializer<'de> {
+
     String::deserialize(d)?.parse().map_err(de::Error::custom)
 }
 
-pub fn string_number_to_bool<'de, D>(d: D) -> Result<bool, D::Error> where D: de::Deserializer<'de> {
+pub fn string_number_to_bool<'de, D>(d: D) -> Result<bool, D::Error>
+    where D: de::Deserializer<'de> {
+
     let num: i32 = String::deserialize(d)?.parse().map_err(de::Error::custom)?;
     Ok(num != 0)
 }
 
-pub fn number_to_bool<'de, D>(d: D) -> Result<bool, D::Error> where D: de::Deserializer<'de> {
+pub fn number_to_bool<'de, D>(d: D) -> Result<bool, D::Error>
+    where D: de::Deserializer<'de> {
+
     let num = u32::deserialize(d)?;
     Ok( num != 0)
 }
